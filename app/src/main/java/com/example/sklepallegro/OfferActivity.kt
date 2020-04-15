bf43bc50be30b957515af0b1561d42b928dd53cd
LucasMsk <lukimssss@gmail.com>
2020-04-15 13:08:36 +0200

    fixes, error handler

---
+++ app/src/main/java/com/example/sklepallegro/OfferActivity.kt
@@ -0,0 +1,37 @@
+package com.example.sklepallegro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.sklepallegro.models.Offer
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val offerViewModel = OfferViewModel()
+        recyclerView.layoutManager =
+            LinearLayoutManager(applicationContext, LinearLayoutManager.VERTICAL, false)
+        val offerListAdapter = OfferListAdapter(applicationContext)
+        recyclerView.adapter = offerListAdapter
+
+        offerViewModel.offerList.observe(this, Observer<List<Offer>> { it ->
+            offerListAdapter.update(it)
+        })
+
+        offerViewModel.errorFlag.observe(this, Observer<Boolean> { it ->
+            if (it) {
+                Toast.makeText(
+                    applicationContext,
+                    R.string.errorFetch,
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        })
+    }
+}
